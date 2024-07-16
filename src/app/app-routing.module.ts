cd7826badcc8347abd22c7cc77646103c76c567c
happyGikundiro <angehappy.gikundiro@amalitechtraining.org>
2024-07-17 01:37:17 +0200

    designed home and how to play pages , implemented the game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GamePlayComponent } from './components/game-play/game-play.component';
-import { GamePausedComponent } from './components/game-paused/game-paused.component';
-import { GameWonComponent } from './components/game-won/game-won.component';
-import { GameFailedComponent } from './components/game-failed/game-failed.component';
 import { CategorySelectionComponent } from './components/category-selection/category-selection.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HowToPlayComponent } from './pages/how-to-play/how-to-play.component';
@@ -13,9 +10,6 @@ const routes: Routes = [
   {path:'howtoplay', component:HowToPlayComponent},
   { path: 'selectcategory', component: CategorySelectionComponent },
   { path: 'game/:category', component: GamePlayComponent },
-  { path: 'paused', component: GamePausedComponent},
-  { path: 'win', component: GameWonComponent },
-  { path: 'lose', component: GameFailedComponent },
 ];
 
 @NgModule({
