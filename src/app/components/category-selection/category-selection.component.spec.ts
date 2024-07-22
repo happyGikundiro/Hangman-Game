c6478789f7fd5cee3d4fc0f8bd9fcbe5d8b1740b
happyGikundiro <angehappy.gikundiro@amalitechtraining.org>
2024-07-22 07:58:27 +0200

    Tested category-selection component

--- src/app/components/category-selection/category-selection.component.spec.ts
+++ src/app/components/category-selection/category-selection.component.spec.ts
@@ -1,23 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { CategorySelectionComponent } from './category-selection.component';
+import { HangmanGameService } from '../../services/hangman-game.service';
+import { Category } from '../../model/model';
+import { CATEGORIES } from '../../db-data';
 
 describe('CategorySelectionComponent', () => {
   let component: CategorySelectionComponent;
   let fixture: ComponentFixture<CategorySelectionComponent>;
+  let routerSpy = { navigate: jest.fn() };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CategorySelectionComponent]
-    })
-    .compileComponents();
+      declarations: [CategorySelectionComponent],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CategorySelectionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should navigate to home when gobackToHome is called', () => {
+    component.gobackToHome();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/']);
+  });
+
+  it('should navigate to game route with selected category when selectCategory is called', () => {
+    const category = 'Animals';
+    component.selectCategory(category);
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/game', category]);
+  });
+
 });
